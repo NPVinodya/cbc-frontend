33024a37a325b3a314da67d077255710948b555d
NPVinodya <prabanivinodya@gmail.com>
2024-12-27 23:07:02 +0530

    Implementing routes and usestate

--- cbc-frontend/src/App.jsx
+++ cbc-frontend/src/App.jsx
@@ -5,14 +5,21 @@ import ProductCard from './components/productCard'
 import UserData from './components/UserData'
 import LoginPage from './Pages/loginPage'
 import HomePage from './Pages/homePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
-     <HomePage/>
+    <BrowserRouter>
+    <Routes path = "/*">
+    <Route path="/" element={<HomePage/>}/>
+    <Route path="/login" element={<LoginPage/>}/>
+    <Route path="/*" element={<h1>404 error</h1>}/>
+    </Routes>
+    
+    </BrowserRouter>
     </>
   )
 }
